62b0ee9c2c6663d1f74565b18339bef51da82283
iqbalFatur87 <iqbalfatur18@gmail.com>
2023-07-31 10:18:00 +0700

    updated Page title methods

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
     <div>
-      <Navbar v-if="shouldShowNavbar" :brandName="brandName" :homeLink="homeLink" :navLinks="navLinks" />
+      <Navbar
+        v-if="shouldShowNavbar"
+        :brandName="brandName"
+        :homeLink="homeLink"
+        :navLinks="navLinks"
+      />
       <router-view :key="$route.fullPath" />
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from './Navbar.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Navbar from "./Navbar.vue";
+
+Vue.use(VueRouter);
 
 export default {
   components: {
@@ -16,44 +25,54 @@ export default {
   },
   data() {
     return {
-      brandName: 'Admin Toko',
-      homeLink: '#',
+      brandName: "Admin Toko",
+      homeLink: "#",
       navLinks: [
-        { to: '/', text: 'Login' },
-        { to: '/home', text: 'Home' },
-        { to: '/transaction', text: 'Transaction' },
-        { to: '/products', text: 'Products' },
-        { to: '/accounts', text: 'Account' },
+        { to: "/", text: "Login" },
+        { to: "/home", text: "Home" },
+        { to: "/transaction", text: "Transaction" },
+        { to: "/products", text: "Products" },
+        { to: "/accounts", text: "Account" },
       ],
       showNavbar: true,
     };
   },
-  updatedPageTitle() {
-    // Set the page title based on the current route
-    switch (this.$route.path) {
-      case '/':
-        document.title = 'Login - Admin Toko';
-        break
-      case '/home':
-        document.title = 'Home - Admin Toko';
-        break;
-      case '/transaction':
-        document.title = 'Transaction - Admin Toko';
-        break;
-      case '/products':
-        document.title = 'Products - Admin Toko';
-        break;
-      case '/accounts':
-        document.title = 'Account - Admin Toko';
-        break;
-      default:
-        document.title = 'Admin Toko';
-        break;        
-    }
+  created() {
+    this.updatePageTitle(); // Call the method to update the page title initially
+  },
+  watch: {
+    $route() {
+      this.updatePageTitle(); // Call the method to update the page title whenever the route changes
+    },
+  },
+  methods: {
+    updatedPageTitle() {
+      // Set the page title based on the current route
+      switch (this.$route.path) {
+        case "/":
+          document.title = "Login - Admin Toko";
+          break;
+        case "/home":
+          document.title = "Home - Admin Toko";
+          break;
+        case "/transaction":
+          document.title = "Transaction - Admin Toko";
+          break;
+        case "/products":
+          document.title = "Products - Admin Toko";
+          break;
+        case "/accounts":
+          document.title = "Account - Admin Toko";
+          break;
+        default:
+          document.title = "Admin Toko";
+          break;
+      }
+    },
   },
   computed: {
     shouldShowNavbar() {
-      const hideNavbarRoutes = ['/'];
+      const hideNavbarRoutes = ["/"];
       return !hideNavbarRoutes.includes(this.$route.path);
     },
   },
